d9a74cca71472faa82bf0715a7438d4943c59fbf
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2024-10-17 22:40:50 +0200

    Use Ubuntu 20.04 for tests (#46)
    * Use Ubuntu 20.04 for tests
    * Reenable tests

--- integration/__tests__/extensions.tests.ts
+++ integration/__tests__/extensions.tests.ts
@@ -3,7 +3,7 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 
-import type { ElectronApplication, Frame, Page } from "playwright";
+import type { ConsoleMessage, ElectronApplication, Frame, Page } from "playwright";
 import { expect } from "@jest/globals";
 import * as utils from "../helpers/utils";
 
@@ -12,14 +12,18 @@ describe("extensions page tests", () => {
   let cleanup: undefined | (() => Promise<void>);
   let frame: Frame;
 
+  const logger = (msg: ConsoleMessage) => console.log(msg.text());
+
   beforeAll(async () => {
     let app: ElectronApplication;
 
     ({ window, cleanup, app } = await utils.start());
-    window.on('console', msg => console.log(msg.text()));
+    window.on('console', logger);
+    console.log('await utils.clickWelcomeButton');
     await utils.clickWelcomeButton(window);
 
     // Navigate to extensions page
+    console.log('await app.evaluate');
     await app.evaluate(async ({ app }) => {
       await app.applicationMenu
         ?.getMenuItemById(process.platform === "darwin" ? "mac" : "file")
@@ -30,62 +34,46 @@ describe("extensions page tests", () => {
 
     // Trigger extension install
     const textbox = window.getByPlaceholder("Name or file path or URL");
-    await textbox.fill(process.env.EXTENSION_PATH);
+    console.log('await textbox.fill');
+    await textbox.fill(process.env.EXTENSION_PATH || "@freelensapp/freelens-node-pod-menu");
     const install_button_selector = 'button[class*="Button install-module__button--"]';
-    await window.waitForSelector(install_button_selector.concat('[data-waiting=false]'));
+    console.log('await window.click [data-waiting=false]');
     await window.click(install_button_selector.concat('[data-waiting=false]'));
-    await window.waitForSelector(install_button_selector.concat('[data-waiting=true]'));
-    await window.waitForSelector(install_button_selector.concat('[data-waiting=false]'));
 
     // Expect extension to be listed in installed list and enabled
+    console.log('await window.waitForSelector div[class*="installed-extensions-module__extensionName--"]');
     const installedExtensionName = await (await window.waitForSelector('div[class*="installed-extensions-module__extensionName--"]')).textContent();
     expect(installedExtensionName).toBe("@freelensapp/freelens-node-pod-menu");
     const installedExtensionState = await (await window.waitForSelector('div[class*="installed-extensions-module__enabled--"]')).textContent();
     expect(installedExtensionState).toBe("Enabled");
-    await window.click('div[class*="close-button-module__closeButton--"][aria-label=Close]');
+    console.log('await window.click i[data-testid*="close-notification-for-notification_"]');
+    await window.click('i[data-testid*="close-notification-for-notification_"]');
+    console.log('await window.click div[class*=[close-button-module__closeButton--"][aria-label="Close"]');
+    await window.click('div[class*="close-button-module__closeButton--"][aria-label="Close"]');
 
     // Navigate to catalog
     await window.waitForSelector('div[data-rbd-draggable-id=catalog-entity]');
     await window.click('div[data-rbd-draggable-id=catalog-entity]');
 
     // Navigate to minikube cluster
-    frame = await utils.lauchMinikubeClusterFromCatalog(window);
+    frame = await utils.launchMinikubeClusterFromCatalog(window);
 
     // Expand workloads menu if not already expanded
     await frame.waitForSelector('div[data-testid=sidebar-item-workloads]');
     if (await frame.locator('div[data-testid=sidebar-item-pods]').count() === 0) {
       await frame.click('div[data-testid=sidebar-item-workloads]');
     }
-  }, 10*60*1000);
+  }, 100*60*1000);
 
   afterAll(async () => {
+    // Cannot log after tests are done.
+    window.off('console', logger);
     await cleanup?.();
   }, 10*60*1000);
 
-  it.skip('adds menu items to the pod details overlay', async () => {
-    // Navigate to pods view
-    await frame.click('div[data-testid=sidebar-item-pods]');
-
-    // Navigate to kube-system namespace
-    await frame.click('div[data-testid=namespace-select-filter]');
-    await frame.waitForSelector('.NamespaceSelectFilterMenu');
-    await frame.click('.Select__option >> text=kube-system');
-
-    // Click on etcd-minikube pod
-    await frame.waitForSelector('text=etcd-minikube');
-    await frame.click('text=etcd-minikube');
-
-    // Wait for pod details to pop out
-    await frame.waitForSelector('span[data-icon-name=close]');
-
-    // Check for expected menu items
-    expect(await frame.locator('li[class="MenuItem"] span[class="title"] >> text="Attach Pod"').count()).toBe(1);
-    expect(await frame.locator('li[class="MenuItem"] span[class="title"] >> text="Shell"').count()).toBe(1);
-    expect(await frame.locator('li[class="MenuItem"] span[class="title"] >> text="Logs"').count()).toBe(1);
-
-    // Close pod details
-    await frame.click('span[data-icon-name=close]');
-  }, 10*60*1000);
+  it('installs an extension', async () => {
+    // Nothing, as only beforeAll is called
+  }, 100*60*1000);
 
   it.skip('adds menu items to the pod actions dropdown', async () => {
     // Navigate to pods view
@@ -113,7 +101,7 @@ describe("extensions page tests", () => {
     await frame.click('div[id="name"][class="TableCell name nowrap sorting"]');
   }, 10*60*1000);
 
-  it.skip('adds menu items to the node details overlay', async () => {
+  it('adds menu items to the node details overlay', async () => {
     // Navigate to nodes view
     await frame.click('div[data-testid=sidebar-item-nodes]');
 
@@ -133,7 +121,7 @@ describe("extensions page tests", () => {
     await frame.click('span[data-icon-name=close]');
   }, 10*60*1000);
 
-  it.skip('adds menu items to the node actions dropdown', async () => {
+  it('adds menu items to the node actions dropdown', async () => {
     // Navigate to nodes view
     await frame.click('div[data-testid=sidebar-item-nodes]');
 
