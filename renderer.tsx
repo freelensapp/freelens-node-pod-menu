142eb109a166b01561902c1fee50aa1944afb1eb
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-02-11 00:24:45 +0100

    Deprecate extension (#61)

--- renderer.tsx
+++ renderer.tsx
@@ -4,49 +4,5 @@
  */
 
 import { Renderer } from "@freelensapp/extensions";
-import { NodeMenu } from "./src/node-menu";
-import { PodAttachMenu } from "./src/attach-menu";
-import { PodShellMenu } from "./src/shell-menu";
-import { PodLogsMenu } from "./src/logs-menu";
-import React from "react";
 
-export default class PodMenuRendererExtension extends Renderer.LensExtension {
-  kubeObjectMenuItems = [
-    {
-      kind: "Node",
-      apiVersions: ["v1"],
-      components: {
-        MenuItem: (
-          props: Renderer.Component.KubeObjectMenuProps<Renderer.K8sApi.Node>,
-        ) => <NodeMenu {...props} />,
-      },
-    },
-    {
-      kind: "Pod",
-      apiVersions: ["v1"],
-      components: {
-        MenuItem: (
-          props: Renderer.Component.KubeObjectMenuProps<Renderer.K8sApi.Pod>,
-        ) => <PodAttachMenu {...props} />,
-      },
-    },
-    {
-      kind: "Pod",
-      apiVersions: ["v1"],
-      components: {
-        MenuItem: (
-          props: Renderer.Component.KubeObjectMenuProps<Renderer.K8sApi.Pod>,
-        ) => <PodShellMenu {...props} />,
-      },
-    },
-    {
-      kind: "Pod",
-      apiVersions: ["v1"],
-      components: {
-        MenuItem: (
-          props: Renderer.Component.KubeObjectMenuProps<Renderer.K8sApi.Pod>,
-        ) => <PodLogsMenu {...props} />,
-      },
-    },
-  ];
-}
+export default class PodMenuRendererExtension extends Renderer.LensExtension {}
